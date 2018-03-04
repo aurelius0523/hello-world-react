a649a79a10d4428810cfb3b59ddb0b5826d0caa1
aurelius0523 <aurelius0523@gmail.com>
2018-03-04 15:58:49 +0800

    test travis

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Wow I created a React Page with CLI. Macam yes, berry geng, go home pls</h1>
+          <h1 className="App-title">Wow I created a React Page with CLI. Added travis too it seems!</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
